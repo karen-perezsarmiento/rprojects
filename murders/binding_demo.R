80302a079eb0295db001147c5b1964451c4a578d
karen-perezsarmiento <karen.perez-sarmiento@concordusa.com>
2019-09-04 11:21:30 -0500

    add binding demo

---
+++ murders/binding_demo.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(ggrepel)
+library(dslabs)
+ds_theme_set()
+data(murders)
+data(polls_us_election_2016)
+
+# join the murders table and US election results table
+tab <- left_join(murders, results_us_election_2016, by = "state")
+
+bind_cols(a = 1:3, b = 4:6)
+tab1<-tab[,1:3]
+tab2<-tab[,4:6]
+tab3<-tab[,7:9]
+new_tab<-bind_cols(tab1,tab2,tab3)
+head(new_tab)
+
+tab1 <- tab[1:2,]
+tab2 <- tab[3:4,]
+bind_rows(tab1,tab2)
