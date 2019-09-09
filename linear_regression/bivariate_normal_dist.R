42d42a95eb20ceafa4f73a3b1b5396f3c6219f99
karen-perezsarmiento <karen.perez-sarmiento@concordusa.com>
2019-09-09 13:14:40 -0500

    added bivariate norm dist file

---
+++ linear_regression/bivariate_normal_dist.R
@@ -0,0 +1,10 @@
+library(Lahman)
+library(dplyr)
+library(tidyverse)
+library(HistData)
+
+galton_heights %>% mutate(z_father = round((father-mean(father))/sd(father))) %>%
+  filter(z_father %in% -2:2) %>%
+  ggplot() +
+  stat_qq(aes(sample=son)) +
+  facet_wrap( ~ z_father)
